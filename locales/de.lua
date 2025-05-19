c23621a189b84b90eddad2853caf8112482085a6
ZeroLayerbot <hexenwerkzerolayer@gmail.com>
2025-05-19 13:22:04 +0200

    Add files via upload

---
+++ locales/de.lua
@@ -0,0 +1,277 @@
+Locales['de'] = {
+	-- DialogBox Name
+	['dialogbox_playerid'] = 'Spieler-ID:',
+	['dialogbox_amount'] = 'Menge:',
+	['dialogbox_amount_ammo'] = 'Munitionmenge:',
+	['dialogbox_vehiclespawner'] = 'Fahrzeug Spawnname:',
+	['dialogbox_xyz'] = 'XYZ Position:',
+
+	-- Menu Default Vars
+	['default_gps'] = 'None',
+	['default_attitude'] = 'Normal',
+	['default_voice'] = 'Normal',
+
+	-- Menu Notification
+	['missing_rights'] = 'Du hast nicht genug ~r~Rechte~w~',
+	['no_vehicle'] = 'Du bist nicht in einem Fahrzeug',
+	['players_nearby'] = 'Kein Spieler in der Umgebung',
+	['amount_invalid'] = 'Ungültige Menge',
+	['in_vehicle_give'] = 'Das Geben von %s im Fahrzeug ist nicht möglich',
+	['in_vehicle_drop'] = 'Das Geben von %s im Fahrzeug ist nicht möglich',
+	['not_usable'] = '%s ist nicht benutzbar.',
+	['not_droppable'] = '%s kann man nicht ablegen',
+	['gave_ammo'] = 'Du hast x %s Munition zu %s gegeben',
+	['no_ammo'] = 'Du hast keine Munition',
+	['not_enough_ammo'] = 'Du hast nicht genug Munition',
+
+
+
+	--Tempomat
+
+	['tempomat'] = 'tempomat',
+
+	['speedlimit_stop'] = 'Tempomat deaktivieren',
+	['speedlimit_60'] = '60 km/h',
+	['speedlimit_80'] = '80 km/h',
+	['speedlimit_100'] = '100 km/h',
+	['speedlimit_130'] = '130 km/h',
+	['speedlimit_150'] = '150 km/h',
+	['speedlimit_200'] = '200 km/h',
+	['speedlimit_250'] = '250 km/h',
+
+	['accessories_no_ears'] = 'Du hast keine Ohren-Accessoires',
+	['accessories_no_glasses'] = 'Du hast keine Brille',
+	['accessories_no_helmet'] = 'Du hast keinen Helm / Hut',
+	['accessories_no_mask'] = 'Du hast keine Maske',
+
+	['admin_noclipon'] = 'NoClip ~g~ aktiviert',
+	['admin_noclipoff'] = 'NoClip ~r~ deaktiviert',
+	['admin_godmodeon'] = 'Godmode ~g~ aktiviert',
+	['admin_godmodeoff'] = 'Godmode ~r~ deaktiviert',
+	['admin_ghoston'] = 'Ghostmode ~g~ aktiviert',
+	['admin_ghostoff'] = 'Ghostmode ~r~ deaktiviert',
+	['admin_vehicleflip'] = 'Auto gedreht',
+	['admin_tpmarker'] = 'Zur Markierung teleportiert!',
+	['admin_nomarker'] = 'Keine Markierung auf der Karte!',
+
+	-- Main Menu
+	['mainmenu_subtitle'] = '~b~Interaktionsmenü',
+	['mainmenu_gps_button'] = 'GPS',
+	['mainmenu_voice_button'] = 'Stimme',
+	['gps'] = 'GPS: ~b~%s',
+	['mainmenu_approach_button'] = 'Gehstil',
+	['approach'] = 'Nähern: ~b~%s',
+	['voice'] = 'Stimme: ~b~%s',
+
+	-- Menu Voice Level
+	['voice_whisper'] = 'Leise',
+	['voice_normal'] = 'Normal',
+	['voice_cry'] = 'Schreien',
+
+	-- Inventory Menu
+	['inventory_title'] = 'Inventar',
+	['inventory_actions_title'] = 'Inventar: Aktionen',
+	['inventory_use_button'] = 'Verwenden',
+	['inventory_give_button'] = 'Geben',
+	['inventory_drop_button'] = 'Entsorgen',
+	['inventory_actions_subtitle'] = 'Inventar: Aktionen',
+
+	-- Loadout Menu
+	['loadout_title'] = 'Waffenmenü',
+	['loadout_actions_title'] = 'Waffen: Aktionen',
+	['loadout_give_button'] = 'Benutzen',
+	['loadout_givemun_button'] = 'Munition geben',
+	['loadout_drop_button'] = 'Entsorgen',
+	['loadout_actions_subtitle'] = 'Waffen: Aktionen',
+
+	-- Wallet Menu
+	['wallet_title'] = 'Brieftasche',
+	['wallet_option_give'] = 'Geben',
+	['wallet_option_drop'] = 'Entsorgen',
+	['wallet_job_button'] = 'Job: %s - %s',
+	['wallet_job2_button'] = 'Firma: %s - %s',
+	['wallet_money_button'] = 'Bargeld: €%s',
+	['wallet_bankmoney_button'] = 'Bankguthaben: €%s',
+	['wallet_blackmoney_button'] = 'Schwarzgeld: €%s',
+	['wallet_show_idcard_button'] = 'Personalausweis zeigen',
+	['wallet_check_idcard_button'] = 'Personalausweis anschauen',
+	['wallet_show_driver_button'] = 'Führerschein zeigen',
+	['wallet_check_driver_button'] = 'Führerschein anschauen',
+	['wallet_show_firearms_button'] = 'Waffenschein zeigen',
+	['wallet_check_firearms_button'] = 'Waffenschein anschauen',
+
+	-- Bills Menu
+	['bills_title'] = 'Rechnungen',
+
+	-- Clothes Menu
+	['clothes_title'] = 'Kleidungsmenü',
+	['clothes_torso'] = 'Oberteil',
+	['clothes_pants'] = 'Hose',
+	['clothes_shoes'] = 'Schuhe',
+	['clothes_bag'] = 'Rucksack / Tasche',
+	['clothes_bproof'] = 'Schusssichere Weste',
+	['clothes_top'] = 'Untershirt',
+
+	-- Accessories Menu
+	['accessories_title'] = 'Zubehör / Accessories',
+	['accessories_ears'] = 'Ohrenschmuck',
+	['accessories_glasses'] = 'Brille',
+	['accessories_helmet'] = 'Hut / Helm',
+	['clothes_mask'] = 'Maske',
+
+	-- Animation Menu
+	['animation_title'] = 'Animationen',
+
+	['animation_party_title'] = 'Festivals',
+	['animation_party_smoke'] = 'Zigarette rauchen',
+	['animation_party_playsong'] = 'Musik abspielen',
+	['animation_party_dj'] = 'DJ',
+	['animation_party_beer'] = 'Bier und Party',
+	['animation_party_dancing'] = 'Party machen',
+	['animation_party_airguitar'] = 'Luftgitarre',
+	['animation_party_shagging'] = 'Luft ficken',
+	['animation_party_rock'] = 'Rock n Roll',
+	['animation_party_drunk'] = 'Auf der Stelle treten',
+	['animation_party_vomit'] = 'Im Fahrzeug kotzen',
+
+	['animation_salute_title'] = 'Szenarien',
+	['animation_salute_saluate'] = 'Grüße',
+	['animation_salute_serrer'] = 'Hände schütteln',
+	['animation_salute_tchek'] = 'Einklatschen',
+	['animation_salute_bandit'] = 'Banditengruß',
+	['animation_salute_military'] = 'Militärischer Gruß',
+
+	['animation_work_title'] = 'Arbeit',
+	['animation_work_suspect'] = 'Aufgeben',
+	['animation_work_fisherman'] = 'Fischer',
+	['animation_work_inspect'] = 'Schriftart: untersuchen',
+	['animation_work_radio'] = 'Schriftart: im Radio sprechen',
+	['animation_work_circulation'] = 'Schriftart: Zirkulation',
+	['animation_work_binoculars'] = 'Schrift: Fernglas',
+	['animation_work_harvest'] = 'Landwirtschaft: Ernte',
+	['animation_work_repair'] = 'Fehlerbehebung: Motor reparieren',
+	['animation_work_observe'] = 'Arzt: beobachten',
+	['animation_work_talk'] = 'Taxi: mit dem Kunden sprechen',
+	['animation_work_bill'] = 'Taxi: Rechnung austellen',
+	['animation_work_buy'] = 'Lebensmittelhändler: Lebensmittel geben',
+	['animation_work_shot'] = 'Barkeeper: Schenke einen Shot aus',
+	['animation_work_picture'] = 'Reporter: Mach ein Foto',
+	['animation_work_notes'] = 'Alle: Notizen machen',
+	['animation_work_hammer'] = 'Alle: Hammerschlag',
+	['animation_work_beg'] = 'SDF: Mach die Runde',
+	['animation_work_statue'] = 'SDF: Mach die Statue',
+
+	['animation_mood_title'] = 'Stimmung',
+	['animation_mood_felicitate'] = 'Herzlichen Glückwunsch',
+	['animation_mood_nice'] = 'Super',
+	['animation_mood_you'] = 'Du',
+	['animation_mood_come'] = 'Komm',
+	['animation_mood_what'] = 'Keskya?',
+	['animation_mood_me'] = 'Für mich',
+	['animation_mood_serious'] = 'Ich wusste es, verdammt',
+	['animation_mood_tired'] = 'Erschöpft sein',
+	['animation_mood_shit'] = 'Ich stecke in der Scheiße',
+	['animation_mood_facepalm'] = 'Gesichtspalme',
+	['animation_mood_calm'] = 'Beruhige dich',
+	['animation_mood_why'] = 'Was habe ich getan?',
+	['animation_mood_fear'] = 'Hab Angst',
+	['animation_mood_fight'] = 'Kampf?',
+	['animation_mood_notpossible'] = 'Das ist nicht möglich!',
+	['animation_mood_embrace'] = 'Umarmen',
+	['animation_mood_fuckyou'] = 'Mittelfinger',
+	['animation_mood_wanker'] = 'Wichser',
+	['animation_mood_suicide'] = 'Kugel im Kopf',
+
+	['animation_sports_title'] = 'Sport',
+	['animation_sports_muscle'] = 'Zeige deine Muskeln',
+	['animation_sports_weightbar'] = 'Gewichtsleiste',
+	['animation_sports_pushup'] = 'Liegestützen machen',
+	['animation_sports_abs'] = 'Sit Ups machen',
+	['animation_sports_yoga'] = 'Yoga machen',
+
+	['animation_other_title'] = 'Verschiedenes',
+	['animation_other_beer'] = 'Bier',
+	['animation_other_sit'] = 'Sitzen',
+	['animation_other_waitwall'] = 'Warten, gegen eine Wand lehnen',
+	['animation_other_ontheback'] = 'Auf den Rücken legen',
+	['animation_other_stomach'] = 'Auf den Bauch legen',
+	['animation_other_clean'] = 'Säubern',
+	['animation_other_cooking'] = 'Kochen',
+	['animation_other_search'] = 'Suchposition',
+	['animation_other_selfie'] = 'Mach ein Selfie',
+	['animation_other_door'] = 'An einer Tür lauschen',
+
+	['animation_pegi_title'] = 'Ab 18 Jahren',
+	['animation_pegi_hsuck'] = 'Mann bekommt Blowjob im Auto',
+	['animation_pegi_fsuck'] = 'Frau lutscht im Auto',
+	['animation_pegi_hfuck'] = 'Mann küsst Schwanz im Auto',
+	['animation_pegi_ffuck'] = 'Frau wird im Auto gefickt',
+	['animation_pegi_scratch'] = 'An den Eiern kratzen',
+	['animation_pegi_charm'] = 'Charmant posieren',
+	['animation_pegi_golddigger'] = 'Schwul posieren',
+	['animation_pegi_breast'] = 'Titten zeigen',
+	['animation_pegi_strip1'] = 'Strip Tease 1',
+	['animation_pegi_strip2'] = 'Strip Tease 2',
+	['animation_pegi_stripfloor'] = 'Strip Tease auf dem Boden',
+
+	['animation_attitudes_title'] = 'Gangart',
+
+	-- Vehicle Menu
+	['vehicle_title'] = 'Fahrzeugmenü',
+	['vehicle_engine_button'] = 'Motor ein-/ausschalten',
+	['vehicle_door_button'] = 'Tür öffnen / schließen',
+	['vehicle_hood_button'] = 'Motorhaube öffnen / schließen',
+	['vehicle_trunk_button'] = 'Kofferraum öffnen / schließen',
+	['vehicle_door_frontleft'] = 'Vorne Links',
+	['vehicle_door_frontright'] = 'Vorne Rechts',
+	['vehicle_door_backleft'] = 'Hinten Links',
+	['vehicle_door_backright'] = 'Hinten Rechts',
+	
+	['vehicle_extramenu'] = 'Extras',
+	['vehicle_extra1'] = 'Extra 1',
+	['vehicle_extra2'] = 'Extra 2',
+	['vehicle_extra3'] = 'Extra 3',
+	['vehicle_extra4'] = 'Extra 4',
+	
+	-- Boss Management Menu
+
+	['bossmanagement_title'] = 'Geschäftsführung',
+	['bossmanagement_chest_button'] = 'Geschäft:',
+	['bossmanagement_hire_button'] = 'Einstellen',
+	['bossmanagement_fire_button'] = 'Entlassen',
+	['bossmanagement_promote_button'] = 'Befördern',
+	['bossmanagement_demote_button'] = 'Degradieren',
+
+	-- Boss Management 2 Menu
+
+	['bossmanagement2_title'] = 'Geschäftsführung 2',
+	['bossmanagement2_chest_button'] = 'Geschäft:',
+	['bossmanagement2_hire_button'] = 'Einstellen',
+	['bossmanagement2_fire_button'] = 'Entlassen',
+	['bossmanagement2_promote_button'] = 'Befördern',
+	['bossmanagement2_demote_button'] = 'Degradieren',
+
+	-- Admin Menu
+	['admin_title'] = '~r~Admintools~s~',
+	['admin_goto_button'] = 'Zu Spieler teleportieren',
+	['admin_bring_button'] = 'Spieler zu mir teleportieren',
+	['admin_tpxyz_button'] = 'Zu Koordinaten teleportieren',
+	['admin_noclip_button'] = 'NoClip',
+	['admin_godmode_button'] = 'Godmode',
+	['admin_ghostmode_button'] = 'Ghostmode',
+	['admin_spawnveh_button'] = 'Fahrzeug spawnen',
+	['admin_repairveh_button'] = 'Fahrzeug reparieren',
+	['admin_flipveh_button'] = 'Fahrzeug umdrehen',
+	['admin_givemoney_button'] = 'Geld auf die Hand geben',
+	['admin_givebank_button'] = 'Geld auf Bankkonto einzahlen',
+	['admin_givedirtymoney_button'] = 'Schwarzgeld geben',
+	['admin_showxyz_button'] = 'Koordinaten anzeigen / ausblenden',
+	['admin_showname_button'] = 'Spielernamen anzeigen / ausblenden',
+	['admin_tpmarker_button'] = 'Zum Wegpunkt teleportieren',
+	['admin_revive_button'] = 'Spieler wiederbeleben',
+	['admin_changeskin_button'] = 'Skin ändern',
+	['admin_saveskin_button'] = 'Skin speichern',
+	
+	--System Menu
+	['server_title'] = '~r~Server~s~'
+}
